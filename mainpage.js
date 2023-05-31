0f03de04eaedc72abef1495a33ec03da2328e333
potatoslc <potatoslc@icloud.com>
2023-05-31 09:42:57 -0600

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
-const dropdown = () =>{
 
-const [selectedOption,setselectedoption ] = useState('');
-const handlechange = (event)=>{
-    setselectedoption(event.target.value);
-};
-return (
+const Dropdown = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+
+    // Send the selected choice to the backend server
+    fetch('/display_choice', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ choice: event.target.value }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
     <div>
-        <h1>dropdown list </h1>
-        <select value= {selectedOption} onChange={handlechange}> 
-            <option value =""> select option</option>
-            <option value = "option1"> Option1</option>
-            <option value = "option2"> Option2</option>
-        </select>
-        <p>selected option: {selectedOption}</p>
+      <h1>Dropdown List Example</h1>
+      <select value={selectedOption} onChange={handleChange}>
+        <option value="">Select Option</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+      </select>
+      <p>Selected Option: {selectedOption}</p>
     </div>
-);
+  );
 };
+
+export default Dropdown;

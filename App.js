6b6c376b346b42436a469e336e9bb2848ce92d6d
potatoslc <potatoslc@icloud.com>
2023-05-31 09:43:41 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dropdown from './Dropdown';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Dropdown />
+    </div>
+  );
+};
+
+export default App;
